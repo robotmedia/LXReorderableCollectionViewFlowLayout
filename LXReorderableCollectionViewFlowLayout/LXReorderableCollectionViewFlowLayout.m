f2e4a97e478df54c5d730a9b5062d9a5b15d252f
Marius Landwehr <mla@basecom.de>
2015-08-03 15:22:21 +0200

    removed strange method in collectionViewContentSize

--- LXReorderableCollectionViewFlowLayout/LXReorderableCollectionViewFlowLayout.m
+++ LXReorderableCollectionViewFlowLayout/LXReorderableCollectionViewFlowLayout.m
@@ -593,20 +593,4 @@ static NSString * const kLXCollectionViewKeyPath = @"collectionView";
     // Do nothing...
 }
 
-- (CGSize)collectionViewContentSize
-{
-    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height - 64;
-    CGSize collectionViewContentSize = [super collectionViewContentSize];
-    if((int)collectionViewContentSize.height % (int)screenHeight != 0)
-    {
-        int numberOfPages = floor(collectionViewContentSize.height / screenHeight);
-        CGSize newContentSize = CGSizeMake(collectionViewContentSize.width, (screenHeight * numberOfPages) + screenHeight);
-        return newContentSize;
-    }
-    else
-    {
-        return [super collectionViewContentSize];
-    }
-}
-
 @end
